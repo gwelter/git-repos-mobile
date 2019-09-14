8db3838fdcc80a003a6cfaffbc487b3f7b4de990
Guilherme Welter <guilherme.a.welter@gmail.com>
2019-09-13 23:02:27 -0300

    Saving and reading from localStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   Container,
@@ -23,6 +24,20 @@ export default class Main extends React.Component {
     users: [],
     loading: false,
   };
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
 
   handleAddUser = async () => {
     const { newUser, users } = this.state;
